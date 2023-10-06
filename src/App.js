29d54d028a35c644a9f4ab05484dd9ee2d4c9f48
anilamurali <anilamurali98@gmail.com>
2023-10-06 08:08:44 +0530

    Main  page added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
@@ -8,7 +9,14 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Main/>
+
+      <Routes>
+         {/* localhost: 3000 - Main component */}
+        <Route path='/' element={<Main/>} />
+         {/* view a particular doctor localhost:3000/view.2 */}
+        
+      </Routes>
+      
       <Footer/>
     </div>
   );
